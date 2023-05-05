213dae0bb6f0c0df82bf8b66e3b5f9db54c1d664
Cristina Tabares <cristina_tabares@hotmail.es>
2023-05-05 18:47:18 +0200

    lab sin css

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,34 @@
 const express = require('express');
 const router = express.Router();
-
+const Movies = require("../models/Movie.model")
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get("/movies", (req, res, next)=>{
+Movies.find()
+.select({title: 1,image:1 })
+.then((response)=>{
+    console.log(response)
+    res.render("movies.hbs",{
+        allMovies: response
+    })
+})
+.catch((error)=>{
+    next(error)
+})
+})
+router.get("/movies/:id",(req, res, next)=>{
+    Movies.findById(req.params.id)
+    .then((response)=>{
+        console.log("mierda")
+       
+         res.render("see.more.hbs",{
+            verMas: response
+         })
+    })
+   .catch((error)=>{
+    next(error)
+   })
+})
 
 module.exports = router;
